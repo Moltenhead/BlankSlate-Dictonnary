33335947d7133d71bc6f6f799da70a5300bdcc98
Charlie Gardai <charlie.gardai@plugandwork.fr>
2019-11-28 17:32:15 +0100

    fix : set fields and not new local variables

--- app/models/aggregate.rb
+++ app/models/aggregate.rb
@@ -10,11 +10,11 @@ class Aggregate < LanguageConcept
   before_save :before_actions
 
   def inherhit_families
-    families = syllables.pluck(families).flatten.uniq.compact
+    self.families = syllables.pluck(families).flatten.uniq.compact
   end
 
   def inherit_concepts
-    concepts = syllables.pluck(concepts).flatten.uniq.compact
+    self.concepts = syllables.pluck(concepts).flatten.uniq.compact
   end
 
   def before_actions
