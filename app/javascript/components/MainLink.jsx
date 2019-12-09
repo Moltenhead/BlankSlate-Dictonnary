b27ecf81ddb8eae79d018bc898bd867246ada678
Moltenhead <ectopneo@gmail.com>
2019-12-09 23:40:29 +0100

    feat : first working version with react

---
+++ app/javascript/components/MainLink.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import pluralize from "pluralize";
+import { capitalize } from "../utilities/string_utils"
+
+export default class MainLink extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render()
+  {
+    const { name } = this.props
+    return(
+      <div className="col-md-4">
+        <Link
+          to={`/${name && pluralize(name)}`}
+          className="btn btn-lg btn-outline-secondary card mb-4 shadow-sm"
+          role="button"
+        >
+          <div className="card-header">
+            <h2>{capitalize(pluralize(name))}</h2>
+          </div>
+          <div className="card-body">
+            <div className="card-text">
+              List of existing {name && pluralize(name)}
+            </div>
+            <div className="d-flex justify-content-end align-items-end">
+              <small className="text-muted">
+                last created 0 mins ago
+              </small>
+            </div>
+          </div>
+        </Link>
+      </div>
+    );
+  }
+}
