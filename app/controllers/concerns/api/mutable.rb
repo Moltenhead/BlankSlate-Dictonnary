1a6a6c0b403757fa9f264829260bb966683127db
Charlie Gardai <charlie.gardai@plugandwork.fr>
2019-12-11 19:06:03 +0100

    rm : action and simplify

--- app/controllers/concerns/api/mutable.rb
+++ app/controllers/concerns/api/mutable.rb
@@ -12,36 +12,16 @@ module Concerns
         # ========================================== #
         # --------------- UTILITIES  --------------- #
         # ========================================== #
-        def mutable_params(*value)
-          @mutable_params = value
+        def mutable_params(value)
+          @mutable_params = (value || [])
         end
 
-        def mutate_valid_params(with_mutable_params = true)
-          final_mutable_params = @mutable_params.clone || []
-          final_mutable_params.concat(@@mutable_params.clone) if with_mutable_params
+        def mutate_valid_params
           ActiveModelSerializers::Deserialization.jsonapi_parse!(
-            params,
-            only: final_mutable_params, key_transform: :unaltered
-          ).merge(@additional_params)
-        end
-
-        # ========================================== #
-        # ---------------- ACTIONS  ---------------- #
-        # ========================================== #
-        def update
-          return render_api_error(:not_found, :object_not_found) unless @instance
-
-          valid_params = mutate_valid_params
-          return render_api_error(:unprocessable_entity, :no_valid_parameters) unless valid_params.present?
-          return render(json: @instance.errors, status: :unprocessable_entity) unless @instance.update(valid_params) && @instance.save
-
-          render(jsonapi: @instance, serializer: @serializer)
-        end
-      end
-
-      module ClassMethods
-        def base_mutable_params(*value)
-          @@base_mutable_params = value || []
+            params.permit!.to_h,
+            only: @mutable_params,
+            key_transform: :unaltered
+          ).merge(@additional_params || {})
         end
       end
     end
