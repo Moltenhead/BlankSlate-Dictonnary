7ed3ba82279bcafc1a4e9b9d68e980e29d91b067
Charlie Gardai <charlie.gardai@plugandwork.fr>
2019-12-12 18:14:45 +0100

    feat : default path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
-Rails.application.routes.draw do
-  root 'homepage#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :runes do
       end
     end
   end
+
+  root 'homepage#index'
+  get '/*path' => 'homepage#index' # redirect to homepage by default
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
