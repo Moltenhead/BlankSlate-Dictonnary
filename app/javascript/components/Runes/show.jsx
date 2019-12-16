3dc991d435fd60268bc9a01783cab76143c2e799
Charlie Gardai <charlie.gardai@plugandwork.fr>
2019-12-16 20:04:43 +0100

    feat : prepare formating

--- app/javascript/components/Runes/show.jsx
+++ app/javascript/components/Runes/show.jsx
@@ -6,11 +6,10 @@ class Rune extends Show {
     super(props);
     this.modelTypes = "runes";
     this.displayingFields = [
-      {name: 'name', type: String},
-      {name: 'short_description', type: String},
-      {name: 'long_description', type: String},
-      {name: 'roman', type: String},
-      {name: 'phonology', type: String}
+      {name: 'short_description', format: 'short'},
+      {name: 'long_description', format: 'long'},
+      {name: 'roman', format: 'short'},
+      {name: 'phonology', format: 'short'}
     ];
   }
 }
