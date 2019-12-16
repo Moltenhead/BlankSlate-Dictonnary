c342988a0cba9b0ba11ac6c38cacbee4a9c3fb79
Charlie Gardai <charlie.gardai@plugandwork.fr>
2019-12-16 19:48:05 +0100

    mv : Runes related components to Runes/*

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// ROOT
 import Home from "../components/Home";
-import Runes from "../components/Runes"
-import Rune from "../components/Rune"
-import NewRune from "../components/NewRune"
+// RUNES
+import Runes from "../components/Runes/index"
+import Rune from "../components/Runes/show"
+import NewRune from "../components/Runes/create"
 
 export default (
   <Router>
     <Switch>
+      {/*ROOT*/}
       <Route path="/" exact component={Home} />
+      {/*RUNES*/}
       <Route path="/runes" exact component={Runes} />
       <Route path="/rune/:id" exact component={Rune} />
       <Route path="/rune" exact component={NewRune} />
